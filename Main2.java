eef1f7ee5f6daa6f441f4d2bc517811e61e9370b
=Sara Bhatnagar <=10017659@sbstudents.org>
2023-12-07 13:53:40 -0500

    change made

---
+++ Main2.java
@@ -0,0 +1,23 @@
+//Lamya & Sara
+
+public class Main2{
+    public static void main(String args[])
+    {
+        int start = Integer.parseInt(args[0]);
+        int end = Integer.parseInt(args[1]);
+
+        Addition2 add=(int x,int y)->
+        {
+            int sum=0;
+            for(int i=x;i<=y;i++)
+            {
+                sum+=i;
+            }
+            return sum;
+        };
+
+        int result = add.addNum(start, end);
+        System.out.println("Sum of numbers from "+start+" to "+end+": " + result);
+
+    }
+}
